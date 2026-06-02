ec6ca142e0ca170ee4dc8fb4b0ce0d758f3c184d
nandha1986 <to.nandhagopal@gmail.com>
2026-06-02 21:19:04 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base: '/indian-travel-viz-2025/',
+   base: '/Indian-travel-viz-2025/',
   server: {
     port: 5173,
     open: true
